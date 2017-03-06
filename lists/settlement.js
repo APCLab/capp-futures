4287c30165a0639e9d90b76f2d4f4ce9d6d71012
Iblis Lin <iblis@hs.ntnu.edu.tw>
2017-03-06 17:31:17 +0800

    [list] settlement, csv support

--- lists/settlement.js
+++ lists/settlement.js
@@ -8,37 +8,50 @@
  *   http://docs.couchdb.org/en/latest/json-structure.html#request-object
  **/
 function(head, req) {
-  start({
-    'headers': {
-      'Content-Type': 'application/json',
-    }
-  });
 
-  send('[');
+  /*
+   * param format: (csv|json)
+  * */
+  var gen = function(format) {
+    return function() {
+      if (format === 'json')
+        send('[');
+      else if (format === 'csv')
+        send('Date,Symbol,Contract\n')
 
-  var near_contract = "";
-  var last_date = "";
-  while (row = getRow()) {
-    var k = row.key;
-    var date = k[0];
-    var contract = k[2];
+      var near_contract = "";
+      var last_date = "";
+      while (row = getRow()) {
+        var k = row.key;
+        var date = k[0];
+        var contract = k[2];
 
-    if (date > last_date) {
-      near_contract = contract;
-      last_date = date;
-    }
-    else if (date === last_date) {
-      if (near_contract > contract) {
-        continue;
+        if (date > last_date) {
+          near_contract = contract;
+          last_date = date;
+        }
+        else if (date === last_date) {
+          if (near_contract > contract) {
+            continue;
+          }
+        }
+        else {
+          send("\"error\"");
+        }
+
+        if (format === 'json') {
+          send(toJSON([date, k[1], near_contract]));
+          send(',\n');
+        }
+        else if (format === 'csv')
+          send(date + ',' + k[1] + ',' + near_contract + '\n')
       }
-    }
-    else {
-      send("\"error\"");
-    }
 
-    send(toJSON([date, k[1], near_contract]));
-    send(',\n');
-  }
+      if (format === 'json')
+        send(']');
+    };
+  };
 
-  send(']');
+  provides('json', gen('json'));
+  provides('csv', gen('csv'));
 }
