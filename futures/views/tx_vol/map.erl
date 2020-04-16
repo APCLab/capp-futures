81e4044e4340ae1fc9029e867b119f3d6bcb9f5a
Iblis Lin <iblis@hs.ntnu.edu.tw>
2020-04-16 15:56:40 +0800

    VolProfile map version

--- futures/views/tx_vol/map.erl
+++ futures/views/tx_vol/map.erl
@@ -22,17 +22,22 @@ fun({Doc}) ->
       )
     )
   end,
+  ToSortedList = fun(D) ->
+    K = lists:sort(dict:fetch_keys(D)),
+    V = lists:map(fun(X) -> dict:fetch(X, D) end, K),
+    lists:zipwith(fun(X, Y) -> [X, Y] end, K, V)
+  end,
 
   StartTime     = 31500, %% 08:45:00 in seconds
   EndTime       = 49500, %% 13:45:00 in seconds
   AfterHourTime = 53100, %% 14:45:00 in seconds
   TimeFrame     = 1,
 
-  GenKbar = fun(Row) ->
+  GenVolProfile = fun(Row) ->
     TimeIndex = lists:seq(StartTime, EndTime - 1, TimeFrame),
 
-    {_, _, Kbars} = lists:foldl(
-      fun(S, {RawData, PrevKbar, Kbars}) ->
+    {_, VolProfile} = lists:foldl(
+      fun(S, {RawData, VolAcc}) ->
         E = S + TimeFrame,
 
         TimeRange =
@@ -43,45 +48,27 @@ fun({Doc}) ->
 
         {Ticks, Tail} = lists:splitwith(TimeRange, RawData),
 
-        %% calculate kbar
-        Kbar = case Ticks of
-          [] ->
-            PrevC = proplists:get_value(c, PrevKbar, 0.0),
-            [{o, PrevC}, {h, PrevC}, {l, PrevC}, {c, PrevC}, {v, 0}];
+        %% calculate volume profile
+        VProf = lists:foldl(
+          fun
+            ([], _) -> skip;
+            ([_, P, Vol | _], VProf) ->
+              dict:update_counter(P, Vol, VProf)
+          end,
+          dict:new(),
+          Ticks
+        ),
 
-          _ -> lists:foldl(
-            fun([_, P, Vol | _], Kbar) ->
-              lists:map(
-                fun({X, Y, F}) ->
-                  {X, F([proplists:get_value(X, Kbar, Y), Y])}
-                end,
-                [
-                  {o, P, fun erlang:hd/1},
-                  {h, P, fun lists:max/1},
-                  {l, P, fun lists:min/1},
-                  {c, P, fun lists:last/1},
-                  {v, Vol, fun lists:sum/1}
-                ]
-              )
-            end,
-            [],
-            Ticks
-          )
-        end,  % Kbar
-
-        OutputK = [SecondsToTime(S) | lists:map(fun({_K,V}) -> V end, Kbar)],
-
-        {Tail, Kbar, [OutputK | Kbars]}
+        {Tail, [[SecondsToTime(S), ToSortedList(VProf)] | VolAcc]}
       end,
       {
         lists:dropwhile(fun([Time|_]) -> ParseTime(Time) < StartTime end, Row),
-        [],
         []
       },
       TimeIndex
     ),
 
-    lists:reverse(Kbars)
+    lists:reverse(VolProfile)
   end,
 
   case Sym of
@@ -91,7 +78,8 @@ fun({Doc}) ->
         [<<"date">>, <<"symbol">>, <<"contract">>]
       ),
       Rs = couch_util:get_value(<<"records">>, Doc, []),
-      Emit(K, GenKbar(Rs));
+      Emit(K, GenVolProfile(Rs));
     _ -> skip
   end
 end.
+
